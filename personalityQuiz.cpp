2e67fdccd03a3f762239d2ecfe07a50bc76b0c3f
Roz <rosalind.stengle@gmail.com>
2017-04-11 17:36:27 -0500

    preliminary completion of answer calculation

--- personalityQuiz.cpp
+++ personalityQuiz.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include <stdio>
+#inclue <map>
 
 //fields
-	//number of questions counter
-						//a vector of questions
-	std::map<int> 		//ordered map for answer ranges
-	int RawScore = 0;
+	int numQuestions; 							//number of questions counter
+	std::vector<std::string> questions;			//a vector of questions
+	std::map<double, std::string> 				//ordered map for answer ranges
+	int rawScore = 0;
+	std::string firstAnswer, secondAnswer;
 
 //read in file function
 	//open a file stream
@@ -25,13 +27,39 @@
 	//once the 2nd number on the line reads '1.0' close on next line
 	
 	
-void calculateScore () {
-	std::cout
+double calculateScore() {
+	std::string userAnswer;
+	for (auto i = questions.begin(); i != questions.end(); ++i) {
+		std::cout << *it << endl;
+		std::cin >> userAnswer;
+		
+		if (userAnswer.equals(firstAnswer)) {
+			rawScore += 1;
+		}
+		else if (userAnswer.equals(secondAnswer)) {
+			rawScore -= 1;
+		}
+		else {
+			std::cout << "Sorry, " << userAnswer 
+			<< " is not a recognized choice. Please try again." << endl;
+	}
+	
+	double score = rawScore/numQuestions;
+	return score;
 	}
 	
 	
+std::string getUserAnswer(double score) {	
+	for (auto i = answers.begin(); i != answers.end(); ++i) {
+		if (i > score) {
+			i--;
+			return answers[*i];
+		}
+}
+	
 void main() {
 	readFile();
-	calculateScore();
-	std::cout << "Your answer is: " << answer << std::endl;
+	double score = calculateScore();
+	std::string userAnswer = getUserAnswer(score);
+	std::cout << "Your answer is: " << userAnswer << std::endl;
 }
