9d4bcfcffc4ebb89997e4ae91d6b9204f1213abe
Roz <rosalind.stengle@gmail.com>
2017-04-11 16:43:20 -0500

    basic outline completed

---
+++ personalityQuiz.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <stdio>
+
+//fields
+	//number of questions counter
+						//a vector of questions
+	std::map<int> 		//ordered map for answer ranges
+	int RawScore = 0;
+
+//read in file function
+	//open a file stream
+
+	//while the line is not blank
+		//read in string until delimiter '? Enter ' and store in the vector
+		//after the delimiter store first answer until ' or '
+		//store second answer
+		//increment question counter
+		//go to the next line (and repeat)
+		
+		//when 'Results: ' is reached, go to the next line
+		//store the first number as the key to a map and the string as its value
+		//
+		
+	//once the 2nd number on the line reads '1.0' close on next line
+	
+	
+void calculateScore () {
+	std::cout
+	}
+	
+	
+void main() {
+	readFile();
+	calculateScore();
+	std::cout << "Your answer is: " << answer << std::endl;
+}
